e07c2a46ae92d07c910f6e95b580b5bdc1155346
Magic3000 <vova28032000@gmail.com>
2021-02-26 14:33:17 +0400

    Cleanup code

--- MasterSeatFixMain.cs
+++ MasterSeatFixMain.cs
@@ -23,61 +23,46 @@ namespace MasterSeatFix
 	}
 	public class MasterSeatFixMain : MelonMod
 	{
-		public override void OnApplicationStart()
-		{
-			MelonCoroutines.Start(MasterSeatFixMain.InitButton());
-		}
+        public override void OnApplicationStart() => MelonCoroutines.Start(InitButton());
 
-		public static IEnumerator InitButton()
+        public static IEnumerator InitButton()
 		{
-			MasterSeatFixMain._sout("Starting MasterSeatFix...", ConsoleColor.Yellow);
-			while (MasterSeatFixMain.quickMenu == null)
-			{
+			_sout("Starting MasterSeatFix...", ConsoleColor.Yellow);
+			while (quickMenu == null)
 				yield return null;
-			}
-			MasterSeatFixMain.MasterSeatFix = MasterSeatFixMain.InstantiateGameobject("back", "");
-			MasterSeatFixMain.CreateButton(MasterSeatFixMain.MasterSeatFix, "Master-seat Fix", "Fix world-master from seating on VRC_Station", 0, new Color(1f, 1f, 1f), 1f, 3f, MasterSeatFixMain.quickMenu.transform.Find("CameraMenu").gameObject.transform, new Action(() =>
+			MasterSeatFix = InstantiateGameobject("back", "");
+			CreateButton(MasterSeatFix, "Master-seat Fix", "Fix world-master from seating on VRC_Station", 0, new Color(1f, 1f, 1f), 1f, 3f, MasterSeatFixMain.quickMenu.transform.Find("CameraMenu").gameObject.transform, new Action(() =>
 			{
-				MasterSeatFixMain.FixMasterSitting();
+				FixMasterSitting();
 			}));
-			MasterSeatFixMain._sout("MasterSeatFix loaded!", ConsoleColor.Cyan);
+			_sout("MasterSeatFix loaded!", ConsoleColor.Cyan);
 			yield break;
 		}
 
 		private static void FixMasterSitting()
 		{
 			Room currentRoom = PhotonNetwork.prop_Room_0;
-			bool flag = currentRoom == null;
-			if (flag)
-			{
-				MasterSeatFixMain._sout("FixMasterSitting currentRoom is null", ConsoleColor.Red);
-			}
+			if (currentRoom == null)
+				_sout("FixMasterSitting currentRoom is null", ConsoleColor.Red);
 			else
 			{
-				System.Collections.Generic.Dictionary<int, Photon.Realtime.Player> ppList = new System.Collections.Generic.Dictionary<int, Photon.Realtime.Player>();
-				foreach (Il2CppSystem.Collections.Generic.KeyValuePair<int, Photon.Realtime.Player> item in currentRoom.prop_Dictionary_2_Int32_Player_0)
-				{
+				var ppList = new System.Collections.Generic.Dictionary<int, Photon.Realtime.Player>();
+				foreach (var item in currentRoom.prop_Dictionary_2_Int32_Player_0)
 					ppList[item.Key] = item.Value;
-				}
-				bool flag2 = ppList == null || ppList.Count == 0;
-				if (flag2)
+				if (ppList == null || ppList.Count == 0)
 				{
-					MasterSeatFixMain._sout("FixMasterSitting photonPlayerList is null", ConsoleColor.Red);
-				}
-				else
+					_sout("FixMasterSitting photonPlayerList is null", ConsoleColor.Red);
+					return;
+                }
+				var arr = ppList.Select(x => x.Key).ToArray();
+				Array.Sort(arr);
+				VRC.Player masterPlayer = GetPlayer(arr.FirstOrDefault());
+				foreach (VRC_StationInternal vrc_station in Resources.FindObjectsOfTypeAll<VRC_StationInternal>())
 				{
-					int[] arr = (from x in ppList
-								 select x.Key).ToArray<int>();
-					Array.Sort<int>(arr);
-					VRC.Player masterPlayer = MasterSeatFixMain.GetPlayer(arr.FirstOrDefault<int>());
-					foreach (VRC_StationInternal vrc_station in Resources.FindObjectsOfTypeAll<VRC_StationInternal>())
+					if (vrc_station != null && vrc_station.prop_Player_0 == masterPlayer)
 					{
-						bool flag3 = vrc_station != null && vrc_station.prop_Player_0 == masterPlayer;
-						if (flag3)
-						{
-							vrc_station.InteractWithStationRPC(false, masterPlayer);
-							MasterSeatFixMain._sout("Forced vrc_station exit for master", ConsoleColor.Cyan);
-						}
+						vrc_station.InteractWithStationRPC(false, masterPlayer);
+						_sout("Forced vrc_station exit for master", ConsoleColor.Cyan);
 					}
 				}
 			}
@@ -85,13 +70,13 @@ namespace MasterSeatFix
 
 		public static VRC.Player GetPlayer(int int32_0)
 		{
-			object players = MasterSeatFixMain.playermanager.field_Private_List_1_Player_0;
+			object players = playermanager.field_Private_List_1_Player_0;
 			object obj = players;
 			VRC.Player result;
 			lock (obj)
 			{
 				VRC.Player player;
-				MasterSeatFixMain.playermanager.field_Private_Dictionary_2_Int32_Player_0.TryGetValue(int32_0, out player);
+				playermanager.field_Private_Dictionary_2_Int32_Player_0.TryGetValue(int32_0, out player);
 				result = player;
 			}
 			return result;
@@ -106,94 +91,37 @@ namespace MasterSeatFix
 
 		internal static Transform InstantiateGameobject(string type, string go = "")
 		{
-			bool flag = type == "back";
-			Transform transform;
-			if (flag)
-			{
-				transform = UnityEngine.Object.Instantiate<GameObject>(MasterSeatFixMain.quickMenu.transform.Find("CameraMenu/BackButton").gameObject).transform;
-			}
-			else
-			{
-				bool flag2 = type == "nameplates";
-				if (flag2)
-				{
-					transform = UnityEngine.Object.Instantiate<GameObject>(MasterSeatFixMain.quickMenu.transform.Find("UIElementsMenu/ToggleHUDButton").gameObject).transform;
-				}
-				else
-				{
-					bool flag3 = type == "block";
-					if (flag3)
-					{
-						transform = UnityEngine.Object.Instantiate<GameObject>(MasterSeatFixMain.quickMenu.transform.Find("NotificationInteractMenu/BlockButton").gameObject).transform;
-					}
-					else
-					{
-						bool flag4 = type == "next";
-						if (flag4)
-						{
-							transform = UnityEngine.Object.Instantiate<GameObject>(MasterSeatFixMain.quickMenu.transform.Find("QuickMenu_NewElements/_CONTEXT/QM_Context_User_Selected/NextArrow_Button").gameObject).transform;
-						}
-						else
-						{
-							bool flag5 = type == "prev";
-							if (flag5)
-							{
-								transform = UnityEngine.Object.Instantiate<GameObject>(MasterSeatFixMain.quickMenu.transform.Find("QuickMenu_NewElements/_CONTEXT/QM_Context_User_Selected/PreviousArrow_Button").gameObject).transform;
-							}
-							else
-							{
-								bool flag6 = type == "emojimenu";
-								if (flag6)
-								{
-									transform = UnityEngine.Object.Instantiate<GameObject>(MasterSeatFixMain.quickMenu.transform.Find("EmojiMenu").gameObject).transform;
-								}
-								else
-								{
-									bool flag7 = type == "EarlyAccessText";
-									if (!flag7)
-									{
-										bool flag8 = !string.IsNullOrEmpty(go);
-										if (flag8)
-										{
-											MasterSeatFixMain._sout(string.Concat(new string[]
-											{
-												"InstantiateGameobject ",
-												type,
-												" for ",
-												go,
-												" is null"
-											}), ConsoleColor.Red);
-										}
-										throw new ArgumentOutOfRangeException(type);
-									}
-									transform = UnityEngine.Object.Instantiate<GameObject>(MasterSeatFixMain.quickMenu.transform.Find("ShortcutMenu/EarlyAccessText").gameObject).transform;
-								}
-							}
-						}
-					}
-				}
-			}
-			return transform;
+			//("InstantiateGameobject for " + type + ", " + go)._sout(Green);
+			if (type == "back")
+				return UnityEngine.Object.Instantiate<GameObject>(quickMenu.transform.Find("CameraMenu/BackButton").gameObject).transform;
+			if (type == "nameplates")
+				return UnityEngine.Object.Instantiate<GameObject>(quickMenu.transform.Find("UIElementsMenu/ToggleHUDButton").gameObject).transform;
+			if (type == "block")
+				return UnityEngine.Object.Instantiate<GameObject>(quickMenu.transform.Find("NotificationInteractMenu/BlockButton").gameObject).transform;
+			if (type == "next")
+				return UnityEngine.Object.Instantiate<GameObject>(quickMenu.transform.Find("QuickMenu_NewElements/_CONTEXT/QM_Context_User_Selected/NextArrow_Button").gameObject).transform;
+			if (type == "prev")
+				return UnityEngine.Object.Instantiate<GameObject>(quickMenu.transform.Find("QuickMenu_NewElements/_CONTEXT/QM_Context_User_Selected/PreviousArrow_Button").gameObject).transform;
+			if (type == "emojimenu")
+				return UnityEngine.Object.Instantiate<GameObject>(quickMenu.transform.Find("EmojiMenu").gameObject).transform;
+			if (type == "EarlyAccessText")
+				return UnityEngine.Object.Instantiate<GameObject>(quickMenu.transform.Find("ShortcutMenu/EarlyAccessText").gameObject).transform;
+			if (!string.IsNullOrEmpty(go))
+				_sout(("InstantiateGameobject " + type + " for " + go + " is null", ConsoleColor.Red));
+			throw new ArgumentOutOfRangeException(type);
 		}
 
 		public static void CreateButton(Transform transform, string text, string tooltip, int textSize, Color color, float x_pos, float y_pos, Transform parent, UnityAction listener)
 		{
-			float x_button = MasterSeatFixMain.quickMenu.transform.Find("UserInteractMenu/WarnButton").localPosition.x - MasterSeatFixMain.quickMenu.transform.Find("UserInteractMenu/BanButton").localPosition.x;
-			float y_button = MasterSeatFixMain.quickMenu.transform.Find("UserInteractMenu/WarnButton").localPosition.x - MasterSeatFixMain.quickMenu.transform.Find("UserInteractMenu/BanButton").localPosition.x;
+			float x_button = quickMenu.transform.Find("UserInteractMenu/WarnButton").localPosition.x - quickMenu.transform.Find("UserInteractMenu/BanButton").localPosition.x;
+			float y_button = quickMenu.transform.Find("UserInteractMenu/WarnButton").localPosition.x - quickMenu.transform.Find("UserInteractMenu/BanButton").localPosition.x;
 			transform.GetComponentInChildren<Text>().text = text;
-			bool flag = tooltip != "null";
-			if (flag)
-			{
+			if (tooltip != "null")
 				transform.GetComponentInChildren<UiTooltip>().field_Public_String_0 = tooltip;
-			}
-			bool flag2 = textSize != 0;
-			if (flag2)
-			{
+			if (textSize != 0)
 				transform.GetComponentInChildren<Text>().fontSize = textSize;
-			}
 			transform.GetComponentInChildren<Text>().color = color;
-			bool flag3 = x_pos == 0f && y_pos == 0f;
-			if (flag3)
+			if (x_pos == 0 && y_pos == 0)
 			{
 				transform.localPosition = transform.localPosition;
 			}
